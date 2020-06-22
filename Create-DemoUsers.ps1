cb246960598b3c265f772f7a4e38aa79f8be2f1e
Daniel Hibbert <daniel.hibbert@microsoft.com>
2020-06-22 16:19:37 -0400

    added demo script + README

---
+++ Create-DemoUsers.ps1
@@ -0,0 +1,128 @@
+param (
+    [parameter(position=0, mandatory=$true)][string]$userPrefix,                # Prefix for user accounts to build usernames
+    [parameter(position=1, mandatory=$true)][int]$userCount,                    # Count of users to create
+    [parameter(position=2, mandatory=$false)][switch]$createGroup = $false,     # Create a group and add all users
+    [parameter(position=3, mandatory=$false)][switch]$createSP = $false         # Create service principal
+)
+
+Clear-Host
+# Test for active connection to AzureAD
+try {
+    $result = Get-AzureADTenantDetail -ErrorAction Stop
+    if (-not $result.ObjectID) {
+        throw"Please login (connect-AzureAD) and set the proper subscription context before proceeding."
+    }
+
+}
+catch {
+    throw "Please login and set the proper subscription context before proceeding."
+}
+
+# If create group true create a new AAD Group
+if ($createGroup){
+    
+    $groupConfig = New-AzureADGroup -Description "$($userPrefix) - Demo Group" `
+        -DisplayName "$($userPrefix)-group" `
+        -MailEnabled $false `
+        -SecurityEnabled $true `
+        -MailNickName "$($userPrefix)-group"
+    $groupConfig
+}
+
+# IF Create Service Principal
+if ($createSP){
+    Import-Module -Name Az.Resources
+    $spName = "sp-"+$userPrefix
+
+    Write-host -ForegroundColor Green "Creating Service Principal"
+    Write-host -ForegroundColor Green "The SP password will be valid for 15 days."
+
+    # Create SP password - Good for 15 days
+    $spPass = [System.Web.Security.Membership]::GeneratePassword(10,2)
+
+    $spCred = New-Object Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential -Property @{`
+        StartDate=get-date; `
+        EndDate=$(Get-date).addDays(15);`
+        Password=$spPass`
+    }
+
+    # Create new service principal
+    $sp = New-AzADServicePrincipal `
+        -DisplayName $spName `
+        -PasswordCredential $spCred
+
+    # Export SP information
+    $spConfig = [PSCustomObject] @{
+        DisplayName = $sp.DisplayName
+        Secret = $spPass
+        ApplicationID = $sp.ApplicationId
+        ID = $sp.Id
+    }
+    $spOutputPath = $userPrefix+"SPOutput.csv"
+    $spConfig | Export-Csv -NoTypeInformation -Path $spOutputPath
+}
+
+# Create user accounts
+Write-host -ForegroundColor Green "Creating User Accounts"
+$userAccountCompleted = @()
+$i = 0
+
+$domainlist = Get-AzureADDomain | Where-Object IsVerified -eq $true
+# IF multipul domains exists prompt
+if ($domainlist.count -gt 1){
+    Write-host -ForegroundColor Yellow "Multiple Domains found. Select desired domain for user UPN:"
+
+    [int]$listittr = 1
+    $domainlist | ForEach-Object {
+        write-host "[$listittr] - $($_.name)"
+        $listittr++
+    }
+
+    [int]$response = $(Read-Host -Prompt "Select Domain")
+    $upnDomain = $domainlist[$($response - 1)].name
+}
+# ELSE - select domain name
+else {
+    $upnDomain = $domainlist.name
+}
+
+# Create Accounts
+do {
+    # Generate Password
+    $pass = [System.Web.Security.Membership]::GeneratePassword(10,2)
+    $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
+    $PasswordProfile.Password = $pass
+
+    # Define account options
+    $useroption = [PSCustomObject] @{
+        DisplayName = $userPrefix+"User"+$i
+        Password = $pass
+        MailNickname = $userPrefix+"User"+$i
+        UPN = $userPrefix+"User"+$i+"@"+$upndomain
+        ObjectID = ""
+    }
+
+    # Create account
+    write-host -ForegroundColor Green "Creating user: $($useroption.displayname)"
+    $accountConfig = New-AzureADUser `
+        -AccountEnabled $true `
+        -DisplayName $useroption.DisplayName `
+        -PasswordProfile $PasswordProfile `
+        -MailNickname $useroption.MailNickname `
+        -UserPrincipalName $useroption.upn
+        
+    if ($createGroup){
+        Add-AzureADGroupMember `
+            -ObjectId $groupConfig.ObjectID `
+            -RefObjectId $accountConfig.ObjectID
+    }
+
+    $useroption.ObjectID = $accountConfig.ObjectID
+    $userAccountCompleted += $useroption
+    $i++
+} while ($i -ne $userCount)
+
+# Export AAD users
+$outputPath = $userPrefix+"userOutput.csv"
+$userAccountCompleted | Format-Table
+$userAccountCompleted | Export-Csv -NoTypeInformation -Path $outputPath
---
+++ README.md
@@ -0,0 +1,37 @@
+# Azure Demo Day - Setup
+## PowerShell script: create-demousers.ps1
+Scripts and processes to automate the build out for a group Azure Demo.
+
+The easiest way to get started using these examples is to used the [Azure Cloud Shell (ACS)](https://shell.azure.com). ACS has all of the tools required to run all of these templates already baked in, without needing to install anything locally to your workstation.
+
+[![Launch Cloud Shell](https://shell.azure.com/images/launchcloudshell.png "Launch Cloud Shell")](https://shell.azure.com/powershell)
+
+**Note:** These are based on a series of basic scripts and procedures I have used to setup demo environments. 
+
+### Azure AD Identities
+
+**Example**:
+
+```powershell
+Create-DemoUsers.ps1 -userPrefix team1 -userCount 10 -createGroup -createSP
+```
+
+|Parameters||
+|-|-|
+|userPrefix|Prefix name for all of the user accounts / groups created by the script.|
+|userCount|The number of accounts to create. The count will be used when generating account names.|
+|createGroup|[switch] Enables the option to create an AAD group and add all team members created. By default no group is created.|
+|createSP|[switch] Create a service principal|
+
+When the script is a completed a series of CSV files are exported to the current user directory with the information on the created identities.
+
+```csv
+# Service Principal
+"DisplayName","Secret","ApplicationID","ID"
+"sp-test15","...","...","..."
+
+# User Accounts
+"DisplayName","Password","MailNickname","UPN","ObjectID"
+"team1User0","n|)LdDM5p5","team1User0","team1User0@...","..."
+"team1User1","l^LWbpR.qK","team1User1","team1User1@...","..."
+```
